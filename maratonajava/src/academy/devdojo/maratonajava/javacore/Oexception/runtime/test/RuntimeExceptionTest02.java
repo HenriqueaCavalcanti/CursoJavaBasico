5840350499f590a09336041fe5535a9ab4ffc928
Henrique Alves <henriquealves_cavalcanti@hotmail.com>
2023-02-05 12:42:57 -0300

    Parte 3 Exception

---
+++ maratonajava/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest02 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Resultado: " + divisao(4, 0));
+
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+        System.out.println("Codigo finalizado");
+    }
+
+    /**
+     *
+     * @param a
+     * @param b - não pode ser zero
+     * @return
+     * @throws IllegalArgumentException
+     */
+    private static int divisao(int a, int b){
+        if (b == 0) {
+            throw new IllegalArgumentException("O denominador não pode ser 0");
+        }
+        return a/b;
+    }
+}
