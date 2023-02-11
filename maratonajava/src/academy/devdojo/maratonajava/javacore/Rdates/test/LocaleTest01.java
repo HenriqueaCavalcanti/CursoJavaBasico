7f904afe6bb7b42e13db16e4dc851bc755fe17f1
Henrique Alves <henriquealves_cavalcanti@hotmail.com>
2023-02-11 20:21:08 -0300

    Locale

---
+++ maratonajava/src/academy/devdojo/maratonajava/javacore/Rdates/test/LocaleTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        //pt-BR
+        Locale locale = new Locale("it", "IT");
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL, locale);
+        System.out.println("Italia: " + df.format(calendar.getTime()));
+
+    }
+}
